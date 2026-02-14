249e6369f93104162272622d8acba7065d01d0f9
NesterovaAnna <nesterova.anna.alex@yandex.ru>
2026-02-14 12:37:53 +0600

    init

---
+++ Practicum/src/test/java/ru/omstu/fitprogwork/PracticumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.omstu.fitprogwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
